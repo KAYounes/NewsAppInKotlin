5a41c07cbe3751b625dfa710b70ceaf1dacdd5c7
Kareem Amr Younes <official.kyounes@gmail.com>
2020-10-04 16:33:58 +0200

    Api endpoints done

--- app/src/main/java/com/example/newsappinkotlin/ApiEndPoints.kt
+++ app/src/main/java/com/example/newsappinkotlin/ApiEndPoints.kt
@@ -1,4 +1,16 @@
 package com.example.newsappinkotlin
 
+import retrofit2.Call
+import retrofit2.http.GET
+import retrofit2.http.Query
+
 interface ApiEndPoints {
+    @GET("v2/top-headlines")
+
+    fun getTopHeadLines(
+        @Query("country") country: String,
+        @Query("apiKey") key: String = "cac7619eebd84f24a1e30ace5c13708b"
+    ): Call<TopHeadLinesResponse>
+
+
 }
---
+++ app/src/main/java/com/example/newsappinkotlin/TopHeadLinesResponse.kt
@@ -0,0 +1,15 @@
+package com.example.newsappinkotlin
+
+import com.google.gson.annotations.SerializedName
+
+data class TopHeadLinesResponse(
+    @SerializedName("source") val headLineSource: Source,
+    @SerializedName("title") val headLineTitle: String ,
+    @SerializedName("urlToImage") val headLineThumbNail: String ,
+    @SerializedName("publishedAt") val published: String
+)
+
+class Source(
+    val id: String?,
+    val name: String
+)
